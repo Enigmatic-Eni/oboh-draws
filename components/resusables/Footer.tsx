22b75dd5a44ca3d898d5c9a881ad29b9940bfaf8
Motunrayo Sanni <sannimotunrayo04@gmail.com>
2024-11-14 16:21:11 +0100

    added social links

--- components/resusables/Footer.tsx
+++ components/resusables/Footer.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <div className=' bg-[#d5d4d3]'>
-        THis is
+    <div className=' bg-[#d5d4d3] flex justify-center space-x-8 text-lg text-[#545353] py-8'>
+        <FaFacebookF className=' hover:text-[#a9bdcf] cursor-pointer'/>
+        <FaInstagram className=' hover:text-[#a9bdcf] cursor-pointer'/>
+        <FaTwitter className=' hover:text-[#a9bdcf] cursor-pointer' />
     </div>
   )
 }
--- components/resusables/ShopPrints.tsx
+++ components/resusables/ShopPrints.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 
 export default function ShopPrints() {
-  return (
-    <div className="bg-[#d5d4d3]">
-        <div className=' flex justify-center'>
-        <button className='bg-[#545353] text-white px-12 text-xl rounded-lg mt-8 mb-14 hover:bg-[#7c7c7c] font-semibold py-4'>Shop Prints Here</button>
-        </div>
-    <div className=' bg-[#d5d4d3] grid-cols-5 grid pl-6'>
-        <img src="/shop-prints/Living in the clouds2.jpg" alt="" className=' h-60 w-56 rounded-2xl'/>
-        <img src="/shop-prints/monday-scaled.jpg" alt="" className=' h-60 w-56 rounded-2xl'/>
-        <img src="/shop-prints/nightmare.jpg" alt="" className=' h-60 w-56 rounded-2xl'/>
-        <img src="/shop-prints/cleanse.jpg" alt="" className=' h-60 w-56 rounded-2xl'/>
-        <img src="/shop-prints/work-train2.jpg" alt="" className=' h-60 w-56 rounded-2xl'/>
+    return (
+        <div className="bg-[#d5d4d3]">
+            <div className=' flex justify-center'>
+                <button className='bg-[#545353] text-white px-12 text-xl rounded-lg mt-8 mb-14 hover:bg-[#7c7c7c] font-semibold py-4'>Shop Prints Here</button>
+            </div>
+            <div className=' bg-[#d5d4d3] grid-cols-5 grid pl-6 pb-14'>
+                <div className=' parent  h-60 w-56 overflow-hidden rounded-lg'>
+                    <img src="/shop-prints/Living in the clouds2.jpg" alt="" className='child h-full w-full   transition-transform duration-300 hover:scale-105 hover:cursor-pointer' />
+                </div>
+                <div className=' parent h-60 w-56 overflow-hidden rounded-lg'>
+                    <img src="/shop-prints/monday-scaled.jpg" alt="" className=' child h-full w-full   transition-transform duration-300 hover:scale-105 hover:cursor-pointer' />
+                </div>
+                <div className=' parent h-60 w-56 overflow-hidden rounded-lg'>
+                    <img src="/shop-prints/nightmare.jpg" alt="" className='child h-full w-full   transition-transform duration-300 hover:scale-105 hover:cursor-pointer' />
+                </div>
+                <div className=' parent h-60 w-56 overflow-hidden rounded-lg'>
+                    <img src="/shop-prints/cleanse.jpg" alt="" className=' child h-full w-full   transition-transform duration-300 hover:scale-105 hover:cursor-pointer' />
+                </div>
+                <div className=' parent h-60 w-56 overflow-hidden rounded-lg'>
+                    <img src="/shop-prints/work-train2.jpg" alt="" className=' child h-full w-full   transition-transform duration-300 hover:scale-105 hover:cursor-pointer' />
+                </div>
 
-    </div>
-    </div>
-  )
+            </div>
+        </div>
+    )
 }
